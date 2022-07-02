99741873ad72be0749ed584652c8426317704441
sebokdas1 <sebok.das.ff@gmail.com>
2022-07-02 21:54:20 +0600

    latest

--- src/componants/Home/Navbar.js
+++ src/componants/Home/Navbar.js
@@ -4,6 +4,9 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './Navbar.css'
+import { FaExternalLinkAlt, FaFreeCodeCamp, FaUser } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowsUpDown, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     const [user] = useAuthState(auth);
@@ -17,22 +20,20 @@ const Navbar = () => {
         <li className='lg:hidden search-input-text'><input type="text" placeholder='Search 8,000+ tutorials' /></li>
         <li className='lg:text-white'><a href='https://www.freecodecamp.org/donate' target="_blank">Donate</a></li>
         <li className='lg:text-white'><a href='https://www.freecodecamp.org/learn' target="_blank">Curriculam</a></li>
-        <li className='lg:text-white'><a href='https://forum.freecodecamp.org' target="_blank">Forum</a></li>
-        <li className='lg:text-white'><a href='https://www.freecodecamp.org/news' target="_blank">News</a></li>
-        <li className='lg:text-white'><a href='https://coderadio.freecodecamp.org' target="_blank">Radio</a></li>
-
-
+        <li className='lg:text-white'><a href='https://forum.freecodecamp.org' target="_blank">Forum <span><FaExternalLinkAlt /></span></a></li>
+        <li className='lg:text-white'><a href='https://www.freecodecamp.org/news' target="_blank">News <span><FaExternalLinkAlt /></span></a></li>
+        <li className='lg:text-white'><a href='https://coderadio.freecodecamp.org' target="_blank">Radio <span><FaExternalLinkAlt /></span></a></li>
+        <li className='lg:text-white'><a href="#">Change Language</a></li>
+        {user && <li className='lg:text-white hidden lg:block'><button onClick={logOut}>Sign Out</button></li>}
         <li className='lg:text-white lg:hidden'>{user ? <button className="btn btn-ghost" onClick={logOut}>Sign Out</button> : <Link to="/signin">Sign in</Link>}</li>
     </>
     return (
         <div className="navbar nav-bar shadow ">
             <div className="navbar-start nav-div">
                 <div className="dropdown">
-
                     <div className='menu-btn lg:hidden'>
                         <button>MENU</button>
                     </div>
-
                     <ul tabIndex="0" className="menu menu-compact dropdown-content menu-list mt-3 p-2 shadow rounded-box w-52">
                         {menuItems}
                     </ul>
@@ -43,8 +44,8 @@ const Navbar = () => {
                     <input className='input-text-search hidden lg:block' type="text" name="" id="" placeholder='Search 8,000+ tutorials' />
                 </div>
 
-                <div>
-                    <Link to="/" className="btn btn-ghost normal-case text-white text-xl">FreeCodeCamp</Link>
+                <div className='lg:justify-center'>
+                    <Link to="/" className="btn btn-ghost normal-case text-white text-xl"> <span className='flex items-center'>FreeCodeCamp <span className='text-2xl ml-2'><FaFreeCodeCamp /></span></span></Link>
                 </div>
 
                 <div className='hidden lg:block'>
@@ -56,7 +57,7 @@ const Navbar = () => {
                             {menuItems}
                         </ul>
                     </div>
-                    <button>Sign in</button>
+                    {user ? <Link className='signin-link' to="/"><FaUser /></Link> : <Link className='signin-link' to="signin">Sign in</Link>}
                 </div>
 
             </div>
