97c8da3de06ca108a2a84b017b4705b6244aa8b0
sebokdas1 <sebok.das.ff@gmail.com>
2022-07-02 10:33:00 +0600

    btn added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './componants/Home/Home';
 import Signin from './componants/authentication/signIn/Signin';
 import SignUp from './componants/authentication/SignUp/SignUp';
+import Courses from './componants/Home/page2/Courses';
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='courses' element={<Courses />}></Route>
         <Route path='signin' element={<Signin />}></Route>
         <Route path='signup' element={<SignUp />}></Route>
       </Routes>
--- src/componants/Home/page1/Intro.js
+++ src/componants/Home/page1/Intro.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { FaAmazon, FaApple, FaMicrosoft, FaSpotify } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import './Intro.css'
 
 
 const Intro = () => {
     return (
         <div>
-            <h1>Learn to code — for free.</h1>
-            <h1>Build projects.</h1>
-            <h1>Earn certifications.</h1>
-            <h4>Since 2014, more than 40,000 freeCodeCamp.org graduates have gotten jobs at tech companies including:
-            </h4>
-            <div className='companys'>
-                <h3><FaApple /></h3>
-                <h3>Google</h3>
-                <h3><span className='microsoft'><FaMicrosoft /></span> Microsoft</h3>
-                <h3><FaSpotify />Spotify</h3>
-                <h3><FaAmazon />Amazon</h3>
+            <div className='  mx-auto pt-12 intro-container'>
+                <h1 >Learn to code — for free.</h1>
+                <h1 >Build projects.</h1>
+                <h1 >Earn certifications.</h1>
+                <h4>Since 2014, more than 40,000 freeCodeCamp.org graduates have gotten jobs at tech companies including:
+                </h4>
+                <div className='companys mt-2'>
+                    <h3><FaApple /></h3>
+                    <h3>Google</h3>
+                    <h3 className="flex items-center"><span className=''><FaMicrosoft /></span> Microsoft</h3>
+                    <h3 className='flex items-center'><FaSpotify />Spotify</h3>
+                    <h3 className='flex items-center'><FaAmazon />Amazon</h3>
+                </div>
+                <div className='link-div mx-auto'>
+                    <Link className='' to="courses">Get started (it's free)</Link>
+                </div>
             </div>
         </div>
     );
