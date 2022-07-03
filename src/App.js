0a7125885fa208a651f7e9e4f1215fa198762cae
sebokdas1 <sebok.das.ff@gmail.com>
2022-07-03 20:41:11 +0600

    require courses

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Home from './componants/Home/Home';
 import Signin from './componants/authentication/signIn/Signin';
 import SignUp from './componants/authentication/SignUp/SignUp';
 import Courses from './componants/Home/page2/Courses';
+import RequireAuth from './componants/authentication/RequireAuth/RequireAuth';
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path='/' element={<Home />}></Route>
-        <Route path='courses' element={<Courses />}></Route>
+        <Route path='courses' element={
+          <RequireAuth>
+            <Courses />
+          </RequireAuth>
+        }></Route>
         <Route path='signin' element={<Signin />}></Route>
         <Route path='signup' element={<SignUp />}></Route>
       </Routes>
--- src/componants/Home/page2/Courses.js
+++ src/componants/Home/page2/Courses.js
@@ -1,16 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
+import Navbar from '../Navbar';
+import { faAnglesRight } from '@fortawesome/free-solid-svg-icons';
 import './Courses.css'
 
 const Courses = () => {
     const [courses, setCourses] = useState([])
     useEffect(() => {
-        fetch('courses.json')
+        fetch('https://freecodecam-p.herokuapp.com/courses')
             .then(res => res.json())
             .then(data => setCourses(data))
     }, [])
-    console.log(courses.length)
     return (
         <div>
+            <Navbar />
             <div className='courses-container mx-auto'>
                 <h1>Welcome to freeCodeCamp.org</h1>
                 <h3>"Ihave not failed. I've just found 10000 ways that won't work."</h3>
@@ -18,8 +21,10 @@ const Courses = () => {
             </div>
             <div className='course-container mx-auto'>
                 {
-                    courses.map(course => <div>
-                        <h2>{course.name} ({course.duration} hours)</h2>
+                    courses?.map(course => <div key={course._id}>
+                        <img src={course.logo} alt="" />
+                        <h2 className='pl-2'>{course.name} ({course.duration} hours) </h2>
+                        <h2 className='hidden lg:block'><FontAwesomeIcon icon={faAnglesRight}></FontAwesomeIcon></h2>
                     </div>)
                 }
             </div>
